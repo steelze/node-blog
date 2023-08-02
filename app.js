e192b1efb8b8ee3b2218d72ca637b3b7b708ab1d
Odunayo Ogungbure <odunayo@seamlesshr.com>
2023-08-02 22:43:03 +0100

    Generate .editorconfig file

--- app.js
+++ app.js
@@ -6,18 +6,18 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    return res.status(200).render('home');
-    res.json({ greeting: 'Hello World' });
+  return res.status(200).render('home');
+  res.json({ greeting: 'Hello World' });
 });
 
 app.use((req, res) => {
-    return res.status(200).render('./errors/404');
+  return res.status(200).render('./errors/404');
 
-    res.status(404).json({ error: 'Page Not Found' });
+  res.status(404).json({ error: 'Page Not Found' });
 });
 
 const PORT = 3000;
 
 app.listen(3000, () => {
-    console.log(`Listening on PORT - ${PORT}`);
+  console.log(`Listening on PORT - ${PORT}`);
 })
